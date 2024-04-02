09d6cad4c1939f1dbfe109d74f56a37b84442295
icad08 <irsyadrm08@gmail.com>
2024-04-02 14:55:34 +0700

    input data mhs

--- output/struct2.cpp
+++ output/struct2.cpp
@@ -13,7 +13,17 @@ struct mahasiswa(){
    DetailAlamat alamat;
     string umur;
 };
-    
+int main (){
+    mahasiswa mhs;
+    cout << "nomor mahasiswa : " ;
+    cin >> mhs.nim;
+    cout << "nama mahasiswa  : ";
+    cin >> mhs.nama;
+    cout << "alamat mahasiswa : ";
+    cin >>  mhs.alamat;
+    cout << "umur mahasiswa   : ";
+    cin >> mhs.umur;
+    }
 
 int main(){
 
