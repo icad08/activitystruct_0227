78ca84f93662e677acfc222612c6290defac922d
icad08 <irsyadrm08@gmail.com>
2024-04-02 15:24:07 +0700

    membuat struct alamat dan mhs

--- struct4.cpp
+++ struct4.cpp
@@ -2,6 +2,18 @@
 #include <string>
 using namespace std;
 
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	DetailAlamat alamat;
+	int umur;
+};
+
 int main(){
-    
+
 }
